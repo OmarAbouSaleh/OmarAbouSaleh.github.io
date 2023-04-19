868ecd5a6b4abb3479fed7774508a1e11e0b6c4c
Omar Abou-Saleh <omar.abou-saleh@edu.sait.com>
2023-04-19 15:26:07 -0600

    Final Changes

--- scripts/booking.js
+++ scripts/booking.js
@@ -1,40 +1,68 @@
-/********* create variables *********/
+document.addEventListener("DOMContentLoaded", function() {
+    /********* create variables *********/
 // useful variables might be: the cost per day, the number of days selected, and elements on the screen that will be clicked or will need to be modified. 
 // Do any of these variables need to be initialized when the page is loaded? 
 // When do they need to be reset or updated?
-
-
-
-
-/********* colour change days of week *********/
+    let CostPerDay = 0;
+    let NumberOfDaysSelected = 0;
+    let dayButtons = document.querySelectorAll(".day-option");
+    let ClearButton = document.getElementById("clear-button");
+    let Full = document.getElementById("full");
+    let Half = document.getElementById("half");
+    let calculatedCost = document.getElementById("calculated-cost");
+    /********* colour change days of week *********/
 // when the day buttons are clicked, we will apply the "clicked" class to that element, and update any other relevant variables. Then, we can recalculate the total cost.
 // added challenge: don't update the dayCounter if the same day is clicked more than once. hint: .classList.contains() might be helpful here!
-
-
-
-
-
-/********* clear days *********/
+    dayButtons.forEach(function(dayButton) {
+        dayButton.addEventListener('click', function(event) {
+            if (!event.target.classList.contains('clicked')) {
+                NumberOfDaysSelected += 1;
+            }
+            event.target.classList.toggle('clicked');
+            dayButton.style.backgroundColor = '#E5AF42';
+            calculateCost();
+        });
+    });
+    /********* clear days *********/
 // when the clear-button is clicked, the "clicked" class is removed from all days, any other relevant variables are reset, and the calculated cost is set to 0.
+    ClearButton.addEventListener('click', function handleClear(event) {
+        dayButtons.forEach(function(dayButton) {
+            dayButton.classList.remove('clicked');
+            Full.classList.remove('clicked');
+            Half.classList.remove('clicked');
+            dayButton.style.backgroundColor = '';
+            Full.style.backgroundColor = '';
+            Half.style.backgroundColor = '';
+        });
+        NumberOfDaysSelected = 0;
+        calculateCost();
+    });
 
-
-
-
-
-
-/********* change rate *********/
+    /********* change rate *********/
 // when the half-day button is clicked, set the daily rate to $20, add the "clicked" class to the "half" element, remove it from the "full" element, and recalculate the total cost.
+    Half.addEventListener('click', function(event) {
+        CostPerDay = 20;
+        Half.style.backgroundColor = '#E5AF42';
+        Full.style.backgroundColor = '';
+        Half.classList.add('clicked');
+        Full.classList.remove('clicked');
+        calculateCost();
+    });
 
-
-
-
+    Full.addEventListener('click', function(event) {
+        CostPerDay = 35;
+        Full.style.backgroundColor = '#E5AF42';
+        Half.style.backgroundColor = '';
+        Full.classList.add('clicked');
+        Half.classList.remove('clicked');
+        calculateCost();
+    });
 // when the full-day button is clicked, the daily rate is set back to $35, the clicked class is added to "full" and removed from "half", and the total cost is recalculated.
 
 
-
-
-
-/********* calculate *********/
+    /********* calculate *********/
 // when a calculation is needed, set the innerHTML of the calculated-cost element to the appropriate value
-
-
+    function calculateCost() {
+        calculatedCost.innerHTML = CostPerDay * NumberOfDaysSelected;
+    }
+})
--- scripts/contact.js
+++ scripts/contact.js
@@ -1,4 +1,13 @@
 // when the "submit-button" is clicked, the contents of the contact-page are replaced with a single <p> element that reads "Thank you for your message" in size 24 font.
+console.log('Script loaded');
+let Message =  "Thank you for your message";
+let ContactPage = document.querySelector(".contact");
 
+document.addEventListener("DOMContentLoaded", function() {
+    let SubmitButton = document.getElementById("submit-button");
+    SubmitButton.addEventListener('click', function(event) {
+        ContactPage.innerHTML = "<p style='font-size: 24px; text-align: center;'>Thank you for your message</p>";
+    });
+});
 // hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
 
